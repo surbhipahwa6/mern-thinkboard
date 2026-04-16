f2c6dabe336585e692e778859a15201b652e8be8
macbook <macbook@macbooks-MacBook-Air.local>
2026-04-16 19:50:19 +0530

    prepared for deployment

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,7 +1,7 @@
 import express from "express"
 import cors from "cors"
 import dotenv from "dotenv"
-
+import path from "path"
 
 import notesRoutes from "./routes/notesRoutes.js"
 import { connectDB } from "./config/db.js"
@@ -13,12 +13,16 @@ const app = express()
 
 dotenv.config()
 const PORT= process.env.PORT|| 5001
+const __dirname=path.resolve()
 
 //middleware
+
+if(process.env.NODE_ENV!=="production"){
 app.use(cors({
  origin: "http://localhost:5173",
 }
 ))
+}
 app.use(express.json()) // middleware for req.body access
 app.use(rateLimiter)
 //simple custom middleware
@@ -28,6 +32,12 @@ app.use(rateLimiter)
 // })
 
 app.use("/api/notes",notesRoutes)
+if(process.env.NODE_ENV==="production"){
+app.use(express.static(path.join(__dirname,"../frontend/dist")))
+app.get("*",(req,res)=>{
+    res.sendFile(path.join(__dirname,"../frontend","dist","index.html"))
+})
+}
 
 
 
